bf6ac27cbae19e3607a1b98cf8d465e308deeb6e
Mathieu Magnaudet <mathieu.magnaudet@enac.fr>
2018-06-05 15:43:47 +0200

    added forgotten file in compilation process
    Signed-off-by: Mathieu Magnaudet <mathieu.magnaudet@enac.fr>

--- src/core/djnn-lib.mk
+++ src/core/djnn-lib.mk
@@ -1,5 +1,5 @@
 lib_ldflags = -lexpat -lcurl -lpthread
-lib_srcs := src/core/syshook/external_source.cpp src/core/core.cpp
+lib_srcs := src/core/syshook/external_source.cpp src/core/syshook/syshook.cpp src/core/core.cpp
 
 lib_srcs += $(shell find src/core/control -name "*.cpp")
 lib_srcs += $(shell find src/core/tree -name "*.cpp")
