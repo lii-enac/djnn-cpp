203009427bc4e37e9cf5e1b36705e28d9a35c512
Magnaudet Mathieu <mathieu.magnaudet@enac.fr>
2018-08-22 18:26:10 +0200

    base - added min, max and bounded-value components
    Signed-off-by: Magnaudet Mathieu <mathieu.magnaudet@enac.fr>

--- src/base/math_functions.cpp
+++ src/base/math_functions.cpp
@@ -163,5 +163,103 @@ namespace djnn
     AbstractSerializer::post_serialize(this);
 
   }
+
+  Min::Min (Process *p, const string &n, double min, double init_val) :
+      BinaryOperator (p, n)
+  {
+    _left = shared_ptr<AbstractProperty> (new DoubleProperty (this, "min", min));
+    _right = shared_ptr<AbstractProperty> (new DoubleProperty (this, "input", init_val));
+    _result = shared_ptr<AbstractProperty> (new DoubleProperty (this, "result", min < init_val ? init_val : min));
+    init_couplings (shared_ptr<Process> (new MinAction (this, "action", _left, _right, _result)));
+    Process::finalize ();
+  }
+
+  void
+  Min::serialize (const string& type) {
+
+    AbstractSerializer::pre_serialize(this, type);
+
+    AbstractSerializer::serializer->start ("base:min");
+    AbstractSerializer::serializer->text_attribute ("id", _name);
+    AbstractSerializer::serializer->int_attribute ("min", std::dynamic_pointer_cast<DoubleProperty> (_left)->get_value ());
+    AbstractSerializer::serializer->int_attribute ("input", std::dynamic_pointer_cast<DoubleProperty> (_right)->get_value ());
+
+    AbstractSerializer::serializer->end ();
+
+    AbstractSerializer::post_serialize(this);
+
+  }
+
+  Max::Max (Process *p, const string &n, double max, double init_val) :
+      BinaryOperator (p, n)
+  {
+    _left = shared_ptr<AbstractProperty> (new DoubleProperty (this, "max", max));
+    _right = shared_ptr<AbstractProperty> (new DoubleProperty (this, "input", init_val));
+    _result = shared_ptr<AbstractProperty> (new DoubleProperty (this, "result", max >= init_val ? init_val : max));
+    init_couplings (shared_ptr<Process> (new MaxAction (this, "action", _left, _right, _result)));
+    Process::finalize ();
+  }
+
+  void
+  Max::serialize (const string& type) {
+
+    AbstractSerializer::pre_serialize(this, type);
+
+    AbstractSerializer::serializer->start ("base:max");
+    AbstractSerializer::serializer->text_attribute ("id", _name);
+    AbstractSerializer::serializer->int_attribute ("max", std::dynamic_pointer_cast<DoubleProperty> (_left)->get_value ());
+    AbstractSerializer::serializer->int_attribute ("input", std::dynamic_pointer_cast<DoubleProperty> (_right)->get_value ());
+
+    AbstractSerializer::serializer->end ();
+
+    AbstractSerializer::post_serialize(this);
+
+  }
+
+  BoundedValue::BoundedValue (Process *p, const string &n, double min, double max, double init_val) :
+      Process (p, n)
+  {
+    _min = shared_ptr<AbstractProperty> (new DoubleProperty (this, "min", min));
+    _max = shared_ptr<AbstractProperty> (new DoubleProperty (this, "max", max));
+    _input = shared_ptr<AbstractProperty> (new DoubleProperty (this, "input", init_val));
+    _result = shared_ptr<AbstractProperty> (new DoubleProperty (this, "result", init_val < min ? min : (init_val > max ? max : init_val)));
+    _action = shared_ptr<BoundedValueAction> (new BoundedValueAction (this, "action", _min, _max,_input, _result));
+    _c_min = make_unique<Coupling> (_min.get (), ACTIVATION, _action.get (), ACTIVATION);
+    _c_max = make_unique<Coupling> (_max.get (), ACTIVATION, _action.get (), ACTIVATION);
+    _c_input = make_unique<Coupling> (_input.get (), ACTIVATION, _action.get (), ACTIVATION);
+    Graph::instance ().add_edge (_min.get (), _action.get ());
+    Graph::instance ().add_edge (_max.get (), _action.get ());
+    Graph::instance ().add_edge (_input.get (), _action.get ());
+    Graph::instance ().add_edge (_action.get (), _result.get ());
+    if (_parent && _parent->state_dependency () != nullptr)
+      Graph::instance ().add_edge (_parent->state_dependency (), _action.get ());
+    Process::finalize ();
+  }
+
+  BoundedValue::~BoundedValue ()
+  {
+    Graph::instance ().remove_edge (_min.get (), _action.get ());
+    Graph::instance ().remove_edge (_max.get (), _action.get ());
+    Graph::instance ().remove_edge (_input.get (), _action.get ());
+    Graph::instance ().remove_edge (_action.get (), _result.get ());
+    if (_parent && _parent->state_dependency () != nullptr)
+      Graph::instance ().remove_edge (_parent->state_dependency (), _action.get ());
+  }
+
+  void
+  BoundedValue::serialize (const string& type)
+  {
+    AbstractSerializer::pre_serialize(this, type);
+
+    AbstractSerializer::serializer->start ("base:bounded-value");
+    AbstractSerializer::serializer->text_attribute ("id", _name);
+    AbstractSerializer::serializer->int_attribute ("min", std::dynamic_pointer_cast<DoubleProperty> (_min)->get_value ());
+    AbstractSerializer::serializer->int_attribute ("max", std::dynamic_pointer_cast<DoubleProperty> (_max)->get_value ());
+    AbstractSerializer::serializer->int_attribute ("input", std::dynamic_pointer_cast<DoubleProperty> (_input)->get_value ());
+
+    AbstractSerializer::serializer->end ();
+
+    AbstractSerializer::post_serialize(this);
+  }
 }
 
--- src/base/math_functions.h
+++ src/base/math_functions.h
@@ -159,4 +159,88 @@ namespace djnn
       virtual ~Abs () {}
       void serialize (const string& type) override;
   };
+
+  class Min : public BinaryOperator
+  {
+    private:
+      class MinAction : public BinaryOperatorAction
+      {
+      public:
+        MinAction (Process *p, const string &n, shared_ptr<AbstractProperty> min, shared_ptr<AbstractProperty> input, shared_ptr<AbstractProperty> result) :
+          BinaryOperatorAction (p, n, min, input, result) { Process::finalize (); }
+        virtual ~MinAction () {}
+        void activate () {
+          if (_parent->get_state () > activated)
+            return;
+          double min = ((DoubleProperty*) _left.get())->get_value ();
+          double input = ((DoubleProperty*) _right.get())->get_value ();
+          _result.get()->set_value ((min <= input ? input : min), true);
+        }
+         void deactivate () {}
+       };
+     public:
+       Min (Process *p, const string &n, double min, double init_val);
+       virtual ~Min () {}
+       void serialize (const string& type) override;
+   };
+
+  class Max : public BinaryOperator
+  {
+    private:
+      class MaxAction : public BinaryOperatorAction
+      {
+      public:
+        MaxAction (Process *p, const string &n, shared_ptr<AbstractProperty> max, shared_ptr<AbstractProperty> input, shared_ptr<AbstractProperty> result) :
+          BinaryOperatorAction (p, n, max, input, result) { Process::finalize (); }
+        virtual ~MaxAction () {}
+        void activate () {
+          if (_parent->get_state () > activated)
+            return;
+          double max = ((DoubleProperty*) _left.get())->get_value ();
+          double input = ((DoubleProperty*) _right.get())->get_value ();
+          _result.get()->set_value ((max >= input ? input : max), true);
+        }
+         void deactivate () {}
+       };
+     public:
+       Max (Process *p, const string &n, double max, double init_val);
+       virtual ~Max () {}
+       void serialize (const string& type) override;
+   };
+
+  class BoundedValue : public Process
+  {
+  private:
+    class BoundedValueAction : public Process
+    {
+    public:
+      BoundedValueAction (Process *p, const string &n, shared_ptr<AbstractProperty> min,
+                          shared_ptr<AbstractProperty> max, shared_ptr<AbstractProperty> input,
+                          shared_ptr<AbstractProperty> result) : Process (p, n), _min (min), _max (max), _input (input), _result (result) {}
+    virtual ~BoundedValueAction () {}
+      void activate () override {
+        double max = ((DoubleProperty*) _max.get())->get_value ();
+        double min = ((DoubleProperty*) _min.get())->get_value ();
+        double input = ((DoubleProperty*) _input.get())->get_value ();
+        double r = input < min ? min : (input > max ? max : input);
+        _result.get()->set_value (r, true);
+      }
+      void deactivate () override {}
+    private:
+      shared_ptr<AbstractProperty> _min;
+      shared_ptr<AbstractProperty> _max;
+      shared_ptr<AbstractProperty> _input;
+      shared_ptr<AbstractProperty> _result;
+    };
+  public:
+    BoundedValue (Process *p, const string &n, double min, double max, double init_val);
+    void activate () override { _c_min->enable(); _c_max->enable (); _c_input->enable (); _action->activation (); };
+    void deactivate () override { _c_min->disable (); _c_max->disable (); _c_input->disable (); _action->deactivation ();};
+    virtual ~BoundedValue ();
+    void serialize (const string& type) override;
+  protected:
+    shared_ptr<AbstractProperty> _min, _max, _input, _result;
+    unique_ptr<Coupling> _c_min, _c_max, _c_input;
+    shared_ptr<Process> _action;
+  };
 }
