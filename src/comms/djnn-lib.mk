53e1df6f7080942403dd269f515effda143cd30b
stephane conversy <conversy@djnn.net>
2018-09-12 16:41:32 +0200

    comms - update ivy installation procedure

--- src/comms/djnn-lib.mk
+++ src/comms/djnn-lib.mk
@@ -2,10 +2,10 @@ lib_djnn_deps = core
 
 
 # Ivy dependancy
-# 1 - download ivy-c on with : svn co https://svn.tls.cena.fr/svn/ivy/ivy-c/trunk
-# 2 - cd ivy-c/trunc/src
-# 3 - make -f Makefile.osx
-# 4 - sudo make -f Makefile.osx install
+# svn co https://svn.tls.cena.fr/svn/ivy/ivy-c/trunk ivy-c
+# cd ivy-c/src
+# make -f Makefile.osx static-libs shared-libs
+# sudo make -f Makefile.osx installlibs includes
 
 ifeq ($(os),Darwin)
 lib_cppflags = -I/opt/local/include/
