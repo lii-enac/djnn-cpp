1b1963327fb29edfa90e235594aa79be41c626b7
Magnaudet Mathieu <mathieu.magnaudet@enac.fr>
2019-01-10 14:45:02 +0100

    gui - changed default style attribute values to comply with SVG spec
    Signed-off-by: Magnaudet Mathieu <mathieu.magnaudet@enac.fr>

--- src/gui/qt/qt_context.cpp
+++ src/gui/qt/qt_context.cpp
@@ -52,7 +52,7 @@ namespace djnn
   }
 
   QtContext::QtContext () :
-      pen (QColor ("darkslategray"), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin), brush (QColor ("lightgray"),
+      pen (QColor ("darkslategray"), 1, Qt::NoPen, Qt::FlatCap, Qt::RoundJoin), brush (QColor ("black"),
                                                                                             Qt::SolidPattern), matrix (), gradientTransform (), font ()
   {
     alpha = 1;
