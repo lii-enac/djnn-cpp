f07f866810dfdd108879c55a7488866fa5cd59bf
Mathieu Magnaudet <mathieu.magnaudet@enac.fr>
2019-02-04 14:31:27 +0100

    gui - activated Frame's touches
    Signed-off-by: Mathieu Magnaudet <mathieu.magnaudet@enac.fr>

--- src/gui/window.cpp
+++ src/gui/window.cpp
@@ -41,6 +41,7 @@ namespace djnn
     _move = new Spike;
     _wheel = new Spike;
     _touches = new Set (this, "touches");
+    _touches->set_activated ();
     _press_x = new DoubleProperty (0);
     _press_y = new DoubleProperty (0);
     _move_x = new DoubleProperty (0);
