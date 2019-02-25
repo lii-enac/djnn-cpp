4aed640596d5d02e0de1ed5c61615391229825fa
conversy@djnn.net <>
2019-02-25 13:38:12 +0100

    gui/cairo - djnn-lib.mk improve pixmap/gl selection

--- src/gui/cairo/djnn-lib.mk
+++ src/gui/cairo/djnn-lib.mk
@@ -1,25 +1,33 @@
 lib_srcs += $(shell find src/gui/cairo -name "*.cpp" -not -path "*gl*")
 
+cairo_backend ?= pixmap
+#cairo_backend ?= gl
+
+ifeq ($(cairo_backend),pixmap)
 lib_cppflags += -I. -I$(LOCALDIR)/include `pkg-config --cflags cairo pango pangocairo` -DDJNN_SDL
 lib_ldflags += `pkg-config --libs cairo pango pangocairo`
+endif
 
-# cairo opengl	
-#cairo_path := /Users/conversy/tmp/cairo-1.16.0
-#cairo_path := /home/lii/Documents/cairo
-# cairo_lib_path := $(cairo_path)/src/.libs
-# lib_srcs += src/gui/cairo/cairo_gl_sdl_window.cpp
-# lib_cppflags += -I$(cairo_path) -I$(cairo_path)/cairo `pkg-config --cflags pangocairo` `pkg-config --cflags pango` -DUSE_GL_ES_VERSION_2_0=1 -Isrc/gui/gl_
-# lib_ldflags += -L$(cairo_lib_path) -lcairo -L/usr/local/Cellar/pango/1.42.4/lib -L/usr/local/Cellar/glib/2.58.3/lib -L/usr/local/opt/gettext/lib \
-#          -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0
-# mac:
-# -lintl -Wl,-framework -Wl,CoreFoundation -L/Users/conversy/src-ext/SwiftShader/build -lEGL 
+ifeq ($(cairo_backend),gl)
+cairo_path ?= /Users/conversy/tmp/cairo-1.16.0
+# #cairo_path ?= /home/lii/Documents/cairo
+cairo_lib_path := $(cairo_path)/src/.libs
+lib_srcs += src/gui/cairo/cairo_gl_sdl_window.cpp
+lib_cppflags += -I$(cairo_path) -I$(cairo_path)/cairo `pkg-config --cflags pangocairo` `pkg-config --cflags pango` -DUSE_GL_ES_VERSION_2_0=1 -Isrc/gui/gl_
+lib_ldflags += -L$(cairo_lib_path) -lcairo -L/usr/local/Cellar/pango/1.42.4/lib -L/usr/local/Cellar/glib/2.58.3/lib -L/usr/local/opt/gettext/lib \
+          -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0
 
-include src/gui/sdl/djnn-lib.mk
+ifeq ($(os),Darwin)
+lib_ldflags += -lintl -Wl,-framework -Wl,CoreFoundation -L/Users/conversy/src-ext/SwiftShader/build -lEGL
+endif
 #temporary
 include src/gui/gl_/djnn-lib.mk
+endif
 
+include src/gui/sdl/djnn-lib.mk
 
-# for cairo opengl
+# -------------------
+# compiling cairo for opengl
 # compile cairo with: (1. mac or 2. linux)
 #		env PKG_CONFIG_PATH=`pwd` ./configure --disable-dependency-tracking --enable-gobject=yes --enable-xcb=no --enable-xlib=no --enable-xlib-xrender=no --enable-gl=yes
 #		env PKG_CONFIG_PATH=`pwd` ./configure --disable-dependency-tracking --enable-gobject=yes --enable-gl=yes
