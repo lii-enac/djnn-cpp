2aa281987318ddaae20e2a1065f5b10146b93521
Mathieu Poirier <mathieu.poirier@enac.fr>
2019-02-27 18:01:28 +0100

    gui - replaced rectangle.cpp auto-generate by script + added delete on dynamic properties (generated)

--- src/gui/shapes/rectangle.cpp
+++ src/gui/shapes/rectangle.cpp
@@ -14,6 +14,7 @@
  *
  */
 
+
 #include "../backend.h"
 #include "../abstract_backend.h"
 #include "shapes.h"
@@ -21,19 +22,19 @@
 
 namespace djnn
 {
-  Rectangle::Rectangle (Process *p, const std::string& n, double x, double y, double w, double h, double rx = 0, double ry = 0) :
-      AbstractGShape (p, n),
-      raw_props{.x=x, .y=y, .width=w, .height=h, .rx=rx, .ry=ry},
-      _cx (nullptr), _cy (nullptr), _cwidth (nullptr), _cheight (nullptr), _crx (nullptr), _cry (nullptr)
+  Rectangle::Rectangle (Process *p, const std::string& n, double x, double y, double width, double height, double rx, double ry):
+    AbstractGShape (p, n),
+    raw_props{.x=x, .y=y, .width=width, .height=height, .rx=rx, .ry=ry},
+    _cx (nullptr), _cy (nullptr), _cwidth (nullptr), _cheight (nullptr), _crx (nullptr), _cry (nullptr)
   {
     set_origin (x, y);
     Process::finalize ();
   }
 
-  Rectangle::Rectangle (double x, double y, double w, double h, double rx = 0, double ry = 0) :
-      AbstractGShape (),
-      raw_props{.x=x, .y=y, .width=w, .height=h, .rx=rx, .ry=ry},
-      _cx (nullptr), _cy (nullptr), _cwidth (nullptr), _cheight (nullptr), _crx (nullptr), _cry (nullptr)
+  Rectangle::Rectangle (double x, double y, double width, double height, double rx, double ry):
+    AbstractGShape (), 
+    raw_props{.x=x, .y=y, .width=width, .height=height, .rx=rx, .ry=ry},
+    _cx (nullptr), _cy (nullptr), _cwidth (nullptr), _cheight (nullptr), _crx (nullptr), _cry (nullptr)
   {
     set_origin (x, y);
   }
@@ -41,71 +42,119 @@ namespace djnn
   Rectangle::~Rectangle ()
   {
     delete _cx;
-    delete _cy;
-    delete _cwidth;
-    delete _cheight;
-    delete _crx;
-    delete _cry;
-  }
+		delete _cy;
+		delete _cwidth;
+		delete _cheight;
+		delete _crx;
+		delete _cry;
 
-  
+    /* origin_x and origin_y are always in _symtable for AbstractGShape */ 
+    if (_symtable.size () > 2) {
+      std::map<std::string, Process*>::iterator it;
+
+      it = _symtable.find ("x");
+			if (it != _symtable.end ())
+				delete it->second;
+
+			it = _symtable.find ("y");
+			if (it != _symtable.end ())
+				delete it->second;
+
+			it = _symtable.find ("width");
+			if (it != _symtable.end ())
+				delete it->second;
+
+			it = _symtable.find ("height");
+			if (it != _symtable.end ())
+				delete it->second;
+
+			it = _symtable.find ("rx");
+			if (it != _symtable.end ())
+				delete it->second;
+
+			it = _symtable.find ("ry");
+			if (it != _symtable.end ())
+				delete it->second;
+    }
+  }
+ 
   Process*
   Rectangle::find_component (const string& name)
   {
     Process* res = AbstractGShape::find_component(name);
     if(res) return res;
 
+    bool prop_Double=false, prop_Int=false, prop_Text=false;
     Coupling ** coupling;
-    double* rawp;
+    double* rawp_Double;
+    int* rawp_Int;
+    typedef string text;
+    text* rawp_Text;
     int notify_mask = notify_none;
-
+    
     if(name=="x") {
       coupling=&_cx;
-      rawp=&raw_props.x;
+      rawp_Double=&raw_props.x;
       notify_mask = notify_damaged_transform;
+      prop_Double=true;
     } else
     if(name=="y") {
       coupling=&_cy;
-      rawp=&raw_props.y;
+      rawp_Double=&raw_props.y;
       notify_mask = notify_damaged_transform;
+      prop_Double=true;
     } else
     if(name=="width") {
       coupling=&_cwidth;
-      rawp=&raw_props.width;
+      rawp_Double=&raw_props.width;
       notify_mask = notify_damaged_geometry;
+      prop_Double=true;
     } else
     if(name=="height") {
       coupling=&_cheight;
-      rawp=&raw_props.height;
+      rawp_Double=&raw_props.height;
       notify_mask = notify_damaged_geometry;
+      prop_Double=true;
     } else
     if(name=="rx") {
       coupling=&_crx;
-      rawp=&raw_props.rx;
+      rawp_Double=&raw_props.rx;
       notify_mask = notify_damaged_geometry;
+      prop_Double=true;
     } else
     if(name=="ry") {
       coupling=&_cry;
-      rawp=&raw_props.ry;
+      rawp_Double=&raw_props.ry;
       notify_mask = notify_damaged_geometry;
+      prop_Double=true;
     } else
     return nullptr;
     
-    DoublePropertyProxy* prop = nullptr; // do not cache
-    res = create_GObj_prop(&prop, coupling, rawp, name, notify_mask);
+    if(prop_Double) {
+      DoublePropertyProxy* prop = nullptr; // do not cache
+      res = create_GObj_prop(&prop, coupling, rawp_Double, name, notify_mask);
+    }
+    else if(prop_Int) {
+      IntPropertyProxy* prop = nullptr; // do not cache
+      res = create_GObj_prop(&prop, coupling, rawp_Int, name, notify_mask);
+    }
+    else if(prop_Text) {
+      TextPropertyProxy* prop = nullptr; // do not cache
+      res = create_GObj_prop(&prop, coupling, rawp_Text, name, notify_mask);
+    }
 
     return res;
   }
 
   void
-  Rectangle::get_properties_values (double &x, double &y, double &w, double &h, double &rx, double &ry)
+  Rectangle::get_properties_values (double& x, double& y, double& width, double& height, double& rx, double& ry)
   {
     x = raw_props.x;
-    y = raw_props.y;
-    w = raw_props.width;
-    h = raw_props.height;
-    rx = raw_props.rx;
-    ry = raw_props.ry;
+		y = raw_props.y;
+		width = raw_props.width;
+		height = raw_props.height;
+		rx = raw_props.rx;
+		ry = raw_props.ry;
   }
 
   void
@@ -113,11 +162,11 @@ namespace djnn
   {
     AbstractGObj::activate ();
     if(_cx) _cx->enable (_frame);
-    if(_cy) _cy->enable (_frame);
-    if(_cwidth) _cwidth->enable (_frame);
-    if(_cheight) _cheight->enable (_frame);
-    if(_crx) _crx->enable (_frame);
-    if(_cry) _cry->enable (_frame);
+		if(_cy) _cy->enable (_frame);
+		if(_cwidth) _cwidth->enable (_frame);
+		if(_cheight) _cheight->enable (_frame);
+		if(_crx) _crx->enable (_frame);
+		if(_cry) _cry->enable (_frame);
   }
 
   void
@@ -125,11 +174,11 @@ namespace djnn
   {
     AbstractGObj::deactivate ();
     if(_cx) _cx->disable ();
-    if(_cy) _cy->disable ();
-    if(_cwidth) _cwidth->disable ();
-    if(_cheight) _cheight->disable ();
-    if(_crx) _crx->disable ();
-    if(_cry) _cry->disable ();
+		if(_cy) _cy->disable ();
+		if(_cwidth) _cwidth->disable ();
+		if(_cheight) _cheight->disable ();
+		if(_crx) _crx->disable ();
+		if(_cry) _cry->disable ();
   }
 
   void
@@ -139,7 +188,6 @@ namespace djnn
       Backend::instance ()->draw_rectangle (this);
     }
   }
-
 
   Process*
   Rectangle::clone ()
