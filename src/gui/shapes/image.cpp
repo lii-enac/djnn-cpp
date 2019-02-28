1cb872683f2b5e2fca6dc9e6a54abcfeb532d76f
Mathieu Poirier <mathieu.poirier@enac.fr>
2019-02-28 11:51:17 +0100

    gui - added delete on dynamic properties for Image

--- src/gui/shapes/image.cpp
+++ src/gui/shapes/image.cpp
@@ -53,6 +53,31 @@ namespace djnn
       delete _cwatcher;
     }
     delete _watcher;
+
+    /* origin_x and origin_y are always in _symtable for AbstractGShape */ 
+    if (_symtable.size () > 2) {
+      std::map<std::string, Process*>::iterator it;
+
+      it = _symtable.find ("x");
+      if (it != _symtable.end ())
+        delete it->second;
+
+      it = _symtable.find ("y");
+      if (it != _symtable.end ())
+        delete it->second;
+
+      it = _symtable.find ("width");
+      if (it != _symtable.end ())
+        delete it->second;
+
+      it = _symtable.find ("height");
+      if (it != _symtable.end ())
+        delete it->second;
+
+      it = _symtable.find ("path");
+      if (it != _symtable.end ())
+        delete it->second;
+    }
   }
 
   Process*
