ef611bbe9baa6c9ed1706d08906cd03b87a5f1b9
stephane conversy <conversy@djnn.net>
2019-08-12 16:31:36 +0200

    fix compile problem on raspbian

--- src/display/sdl/djnn-lib-flags.mk
+++ src/display/sdl/djnn-lib-flags.mk
@@ -1,6 +1,6 @@
 ifneq ($(cross_prefix),em)
-lib_cppflags += `pkg-config --cflags sdl2 sdl2_image` -DDJNN_SDL
-lib_ldflags += `pkg-config --libs sdl2 sdl2_image`
+lib_cppflags += `pkg-config --cflags sdl2 SDL2_image` -DDJNN_SDL
+lib_ldflags += `pkg-config --libs sdl2 SDL2_image`
 else
-lib_cppflags += -DDJNN_SDL	
+lib_cppflags += -DDJNN_SDL
 endif
