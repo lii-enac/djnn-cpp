648b44a7a0ddd33d5c353d293090f11814bd1365
stephane conversy <conversy@gmail.com>
2020-01-06 15:19:13 +0100

    utils/debug.h - add debug macros

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -1,6 +1,12 @@
 #pragma once
 
-#define __FL__ " " << __FILE__ << ":" << __LINE__ << std::endl;
-#define DBG std::cerr << __FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl;
+#ifdef __PRETTY_FUNCTION__
+#define __DJNN_FUNCTION__ __PRETTY_FUNCTION__
+#else
+#define __DJNN_FUNCTION__ __FUNCTION__
+#endif
 
+#define __FL__ " " << __DJNN_FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl;
+#define DBG std::cerr << __DJNN_FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl;
+#define UNIMPL { static int done=false; if(!done) {std::cerr << "unimplemented " << __DJNN_FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl; done=true;} }
 
