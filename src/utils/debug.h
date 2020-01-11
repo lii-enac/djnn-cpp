2303a601d8f9210c1a037e8258a90870b7ff8b8d
stephane conversy <conversy@gmail.com>
2020-01-11 16:53:39 +0100

    utils/debug.h

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#ifndef __FL__
+
 #ifdef __PRETTY_FUNCTION__
 #define __DJNN_FUNCTION__ __PRETTY_FUNCTION__
 #else
@@ -11,3 +13,5 @@
 #define DBGPROC std::cerr << this << " " << get_name() << " " << __DJNN_FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl;
 #define UNIMPL { static int done=false; if(!done) {std::cerr << "unimplemented " << __DJNN_FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl; done=true;} }
 #define DBGVAR(x) #x << ":" << x
+
+#endif
