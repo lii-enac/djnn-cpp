338d7042a14daa5ff303aec3cfda5090ed7f3708
stephane conversy <conversy@gmail.com>
2020-01-20 18:11:05 +0100

    css-parser - tentative to prevent multiple compile of css parser at the same time

--- src/gui/djnn-lib.mk
+++ src/gui/djnn-lib.mk
@@ -9,12 +9,12 @@ lib_srcs += $(shell find src/gui/widgets -name "*.cpp")
 lib_srcs += src/gui/css-parser/parser.cpp src/gui/css-parser/scanner.cpp src/gui/css-parser/driver.cpp 
 
 $(build_dir)/src/gui/css-parser/scanner.o: CXXFLAGS += -Dregister=""
-$(build_dir)/src/gui/css-parser/%.o: $(build_dir)/src/gui/css-parser/parser.hpp
+#$(build_dir)/src/gui/css-parser/%.o: $(build_dir)/src/gui/css-parser/parser.hpp
 $(build_dir)/src/gui/css-parser/%.o: CXXFLAGS += -I$(build_dir)/src/gui/css-parser -Isrc/gui/css-parser
 
 # for initial make -j
 # find build/src/gui/css-parser -name "*.d" | xargs grep -s "parser.hpp" | awk '{print $1}' | awk -F "." '{print $1".o"}' | sed s/build/\$\(build_dir\)/ | xargs echo
-$(build_dir)/src/gui/css-parser/scanner.o $(build_dir)/src/gui/css-parser/parser.o $(build_dir)/src/gui/css-parser/driver.o: $(build_dir)/src//gui/css-parser/parser.hpp
+#$(build_dir)/src/gui/css-parser/scanner.o $(build_dir)/src/gui/css-parser/parser.o $(build_dir)/src/gui/css-parser/driver.o: $(build_dir)/src/gui/css-parser/parser.hpp
 
 
 ifeq ($(graphics),QT)
