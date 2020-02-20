a49d4abb815514d70723b31bab2e51ab3ce25097
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-02-20 18:09:35 +0100

    core - fixed remove of remotery

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -17,7 +17,7 @@
 #include "core-dev.h"
 #include "core/syshook/syshook.h"
 #include "core/syshook/djnn_time_manager.h"
-#include "core/utils/ext/remotery/Remotery.h"
+//#include "core/utils/ext/remotery/Remotery.h"
 
 #include <locale.h>
 
@@ -29,7 +29,7 @@ namespace djnn
 
   static bool __module_initialized = false;
 
-  Remotery* rmt;
+  //Remotery* rmt;
 
   void
   init_core ()
@@ -47,7 +47,7 @@ namespace djnn
       setlocale(LC_NUMERIC, "C");
     }
 
-    rmt_CreateGlobalInstance(&rmt);
+    //rmt_CreateGlobalInstance(&rmt);
     DjnnTimeManager::instance().cleanup ();
   }
 
@@ -55,6 +55,6 @@ namespace djnn
   clear_core ()
   {
     XML::clear_xml_parser ();
-    rmt_DestroyGlobalInstance(rmt);
+    //rmt_DestroyGlobalInstance(rmt);
   }
 }
