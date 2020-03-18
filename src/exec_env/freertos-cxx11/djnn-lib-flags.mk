dfdab90b5ac173889d04bab868e4382271cf3b8d
stephane conversy <conversy@gmail.com>
2020-03-18 22:09:58 +0100

    exec_env/freertos - adapt to latest freertos and crazyflie firmware

--- src/exec_env/freertos-cxx11/djnn-lib-flags.mk
+++ src/exec_env/freertos-cxx11/djnn-lib-flags.mk
@@ -1,7 +1,7 @@
 lib_cppflags += \
 	-include src/exec_env/freertos-cxx11/freertos-cxx11-macros.h \
 	-Isrc/exec_env/freertos-cxx11 \
-	-I$(freertos_dir)/include -I$(freertos_config_dir) \
+	-I$(freertos_dir)/include -I$(freertos_dir)/portable/GCC/ARM_CM4F -I$(freertos_config_dir) \
 	-I../crazyflie-firmware/src/drivers/interface -I../crazyflie-firmware/src/hal/interface -I../crazyflie-firmware/src/utils/interface \
-	-I../crazyflie-firmware/src/modules/interface -I../crazyflie-firmware/src/lib/FreeRTOS/portable/GCC/ARM_CM4F \
+	-I../crazyflie-firmware/src/modules/interface  \
 	-DDJNN_USE_FREERTOS=1
--- src/exec_env/freertos-cxx11/freertos-cxx11-macros.h
+++ src/exec_env/freertos-cxx11/freertos-cxx11-macros.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 1
-    
+#define configUSE_RECURSIVE_MUTEXES 1 // [SCO]
+
 #define pdMS_TO_TICKS( xTimeInMs ) \
      ( ( TickType_t ) ( ( ( TickType_t ) ( xTimeInMs ) * \
      ( TickType_t ) configTICK_RATE_HZ ) / ( TickType_t ) 1000 ) )
