cf35003d1322991cb472377eace0d220f20e72a5
stephane conversy <conversy@gmail.com>
2020-03-18 21:42:26 +0100

    crazyflie - small changes

--- src/extra/crazyflie/crazyflie_rc.cpp
+++ src/extra/crazyflie/crazyflie_rc.cpp
@@ -22,8 +22,8 @@ namespace djnn {
 	_takeoff(this, "takeoff"),
 	_takeoff_action(this, "takeoff_action"),
 	_c_takeoff (&_takeoff, ACTIVATION, &_takeoff_action, ACTIVATION, true),
-	 _land(this,"land"),
-	 _land_action(this, "land_action"),
+	_land(this,"land"),
+	_land_action(this, "land_action"),
 	_c_land (&_land, ACTIVATION, &_land_action, ACTIVATION, true)
 	{
 		finalize_construction(parent, name);
@@ -37,7 +37,7 @@ namespace djnn {
 		_crazyflie = new Crazyflie (_uri.get_string_value (), djnnLogger);
 		std::cout << "firmware version: " << _crazyflie->getFirmwareVersion() << std::endl;
     	std::cout << "protocol version: " << _crazyflie->getProtocolVersion() << std::endl;
-    	//_crazyflie->sendSetpoint(0,0,0,0);
+    	//_crazyflie->sendSetpoint(0,0,0,0); // unlock
     	_crazyflie->setParamByName("commander", "enHighLevel", (unsigned char)1);
     	//_crazyflie->syson();
     	//std::cout << "device type name: " << _crazyflie->getDeviceTypeName() << std::endl;
@@ -51,15 +51,15 @@ namespace djnn {
 
 	void
 	CrazyflieRC::takeoff()
-	{ DBG;
-		_crazyflie->takeoff(0.5, 5);
+	{ //DBG;
+		_crazyflie->takeoff(0.3, 1);
 		//_crazyflie->sendSetpoint(0,0,0,2);
 	}
 
 	void
 	CrazyflieRC::land()
-	{ DBG;
-		_crazyflie->land(0.1, 2);
+	{ //DBG;
+		_crazyflie->land(0.0, 2);
 		//_crazyflie->sendSetpoint(0,0,0,0);
 	}
 
