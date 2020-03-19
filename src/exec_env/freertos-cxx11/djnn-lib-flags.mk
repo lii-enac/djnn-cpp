9592af1d91c905b44a7b24a90f612d7b96e5cf17
stephane conversy <conversy@gmail.com>
2020-03-19 08:54:49 +0100

    exec_env/freertos - djnn-lib.mk

--- src/exec_env/freertos-cxx11/djnn-lib-flags.mk
+++ src/exec_env/freertos-cxx11/djnn-lib-flags.mk
@@ -1,7 +1,13 @@
+local_dir := $(src_dir)/exec_env
+
 lib_cppflags += \
-	-include src/exec_env/freertos-cxx11/freertos-cxx11-macros.h \
-	-Isrc/exec_env/freertos-cxx11 \
-	-I$(freertos_dir)/include -I$(freertos_dir)/portable/GCC/ARM_CM4F -I$(freertos_config_dir) \
-	-I../crazyflie-firmware/src/drivers/interface -I../crazyflie-firmware/src/hal/interface -I../crazyflie-firmware/src/utils/interface \
-	-I../crazyflie-firmware/src/modules/interface  \
-	-DDJNN_USE_FREERTOS=1
+	-DDJNN_USE_FREERTOS=1 \
+	-include $(local_dir)/freertos-cxx11/freertos-cxx11-macros.h \
+	-I$(local_dir)/freertos-cxx11 \
+	-I$(freertos_dir)/include \
+	-I$(freertos_dir)/portable/GCC/ARM_CM4F \
+	-I$(freertos_config_dir) \
+	-I$(freertos_layers_dir)/drivers/interface \
+	-I$(freertos_layers_dir)/hal/interface \
+	-I$(freertos_layers_dir)/utils/interface \
+	-I$(freertos_layers_dir)/modules/interface
