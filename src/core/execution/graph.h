fb6aa6b0a3509a6671d607cceb25dda73ef0edae
stephane conversy <conversy@gmail.com>
2020-03-25 14:16:32 +0100

    core - still working on reducing size; removing stuff etc

--- src/core/execution/graph.h
+++ src/core/execution/graph.h
@@ -22,9 +22,10 @@
 
 #if !defined(DJNN_NO_DEBUG) || !defined(DJNN_NO_SERIALIZE)
 #include <iostream>
+#endif
+
 //#define DBGG std::cerr << "'" << __FUNCTION__ << " calling graph exec " << __FILE__ << ":" << __LINE__ << std::endl;
 #define DBGG
-#endif
 
 #define GRAPH_EXEC { DBGG; djnn::Graph::instance ().exec (); }
 
