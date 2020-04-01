e1a79ff5b49d49f3d23c5171fabac33c6c914946
stephane conversy <conversy@gmail.com>
2020-04-01 18:57:24 +0200

    DISPLAY/qt - verbosity level for moc

--- src/display/qt/djnn-lib-srcs.mk
+++ src/display/qt/djnn-lib-srcs.mk
@@ -1,7 +1,16 @@
 local_dir := src/display/qt
 
+#$(build_dir)/$(local_dir)/moc_MyQWindow.cpp: $(local_dir)/my_qwindow.h
+#	$(moc) $< > $@
+
 $(build_dir)/$(local_dir)/moc_MyQWindow.cpp: $(local_dir)/my_qwindow.h
+ifeq ($V,max)
 	$(moc) $< > $@
+else
+	@$(call rule_message,compiling,$(stylized_target))
+	@$(moc) $< > $@
+endif
+
 
 lib_objs += $(build_dir)/$(local_dir)/moc_MyQWindow.o
 lib_srcgens += $(build_dir)/$(local_dir)/moc_MyQWindow.cpp
