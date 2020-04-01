19c6af6beac507b74ee36b254b48e4d1441441cf
stephane conversy <conversy@gmail.com>
2020-04-01 18:52:24 +0200

    gui/qt - verbosity level for moc

--- src/gui/qt/qt/djnn-lib-srcs.mk
+++ src/gui/qt/qt/djnn-lib-srcs.mk
@@ -1,8 +1,12 @@
 local_dir := src/gui/qt/qt
 
 $(build_dir)/$(local_dir)/moc_MyQQWindow.cpp: $(local_dir)/my_qqwindow.h
+ifeq ($V,max)
 	$(moc) $< > $@
+else
+	@$(call rule_message,compiling,$(stylized_target))
+	@$(moc) $< > $@
+endif
 
 lib_objs += $(build_dir)/$(local_dir)/moc_MyQQWindow.o
 lib_srcgens += $(build_dir)/$(local_dir)/moc_MyQQWindow.cpp
-#lib_srcs += $(shell find $(local_dir) -name "*.cpp")
