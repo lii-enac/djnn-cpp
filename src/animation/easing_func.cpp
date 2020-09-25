3750b87c7afcdfd9fe3319f9dc44340d6ac66945
Magnaudet Mathieu <mathieu.magnaudet@enac.fr>
2020-09-25 15:46:20 +0200

    animation - fixed wrong calculation
    Signed-off-by: Magnaudet Mathieu <mathieu.magnaudet@enac.fr>

--- src/animation/easing_func.cpp
+++ src/animation/easing_func.cpp
@@ -201,18 +201,17 @@ namespace djnn
   out_bounce (double x) {
     double n1 = 7.5625;
     double d1 = 2.75;
-
-    if (x < 1 / d1) {
-      return n1 * x * x;
-    } else if (x < 2 / d1) {
-      x -= 1.5;
-      return n1 * (x / d1) * x + 0.75;
-    } else if (x < 2.5 / d1) {
-       x -= 2.25;
-      return n1 * (x / d1) * x + 0.9375;
+    if (x < (1 / d1)) {
+      return   n1 * x * x;
+    } else if (x < (2 / d1)) {
+      x = x - (1.5 / d1);
+      return n1 * x * x + 0.75;
+    } else if (x < (2.5 / d1)) {
+       x = x - (2.25 / d1);
+       return n1 * x * x + 0.9375;
     } else {
-      x -= 2.625;
-      return n1 * (x / d1) * x + 0.984375;
+      x = x - (2.625 / d1);
+      return n1 * x * x + 0.984375;
     }
   }
 
