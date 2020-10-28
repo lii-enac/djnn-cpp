292173f98aa63a52cb6917cef187f5c4c00db78b
stephane conversy <conversy@gmail.com>
2020-10-28 15:13:23 +0100

    physics - add missing file

---
+++ src/physics/abstract_pobj.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "core/tree/int_property.h"
+#include "core/tree/double_property.h"
+#include "core/tree/bool_property.h"
+#include "core/tree/text_property.h"
+#include "core/ontology/coupling.h"
+
+namespace djnn {
+
+    // should be world ??
+    class PhysicScene : public CoreProcess {
+		public:
+		CoreProcess* damaged() { return nullptr; }
+	};
+    class AbstractPObj : public FatProcess {
+	public:
+		AbstractPObj (ParentProcess* parent, const std::string& name) : FatProcess(name) {}
+        //World * get_frame () { return get_world (); } // FIXME
+        PhysicScene * get_frame () { return nullptr; } // FIXME
+        virtual void impl_activate () override;
+    	virtual void impl_deactivate () override;
+    virtual FatProcess* create_GObj_prop (BoolPropertyProxy **prop, CouplingWithData  **cprop, bool *rawp, const std::string& name, int notify_mask);
+    virtual FatProcess* create_GObj_prop (IntPropertyProxy **prop, CouplingWithData  **cprop, int *rawp, const std::string& name, int notify_mask);
+    virtual FatProcess* create_GObj_prop (DoublePropertyProxy **prop, CouplingWithData  **cprop, double *rawp, const std::string& name, int notify_mask);
+    virtual FatProcess* create_GObj_prop (TextPropertyProxy **prop, CouplingWithData  **cprop, std::string *rawp, const std::string& name, int notify_mask);
+  };
+}
