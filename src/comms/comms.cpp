980c7e9a90eb93c58f80c0166c7e79dbe787b16f
Mathieu Magnaudet <mathieu.magnaudet@enac.fr>
2020-11-16 14:32:23 +0100

    comms - yet another fix

--- src/comms/comms.cpp
+++ src/comms/comms.cpp
@@ -22,7 +22,9 @@
   #endif
   #include <winsock2.h>
   #include <ws2tcpip.h>
+  #pragma comment(lib, "Ws2_32.lib")
 #endif
+
 
 namespace djnn
 {
@@ -51,7 +53,7 @@ namespace djnn
     }
 #ifdef _WIN32
       WSADATA wsa_data;
-      return WSAStartup(MAKEWORD(1,1), &wsa_data);
+      WSAStartup(MAKEWORD(2,2), &wsa_data);
 #endif
   }
   //TODO We should clean winsock
