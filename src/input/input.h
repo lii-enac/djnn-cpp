2b769d67c22bd9da8b65c0621bf116091def59b5
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-11-30 17:07:38 +0100

    input - added note to access udev file on linux

--- src/input/input.h
+++ src/input/input.h
@@ -24,3 +24,22 @@ namespace djnn {
  	void init_input ();
   
 }
+
+
+/*
+note on udev/evdev :
+
+Accessing to the device files
+
+By default, for security reasons, a non-root user does not have read/write permission to the device files managed by the udevd daemon. This is an issue for the djnn's input module. One solution consists in creating a udev rule that will set the correct permissions to a specific group: 
+- Create a new input group: sudo groupadd -f input
+- Add the current user to this group: sudo gpasswd -a <username> input
+- Create the file (the location may be distro-dependent) : sudo touch /etc/udev/rules.d/99-input.rules
+- with the following content
+
+ KERNEL=="event*", NAME="input/%k", MODE="660",GROUP="input"
+
+- Restart the udev daemon: sudo /etc/init.d/udev restart  or  reboot
+- reboot the computer 
+
+*/
