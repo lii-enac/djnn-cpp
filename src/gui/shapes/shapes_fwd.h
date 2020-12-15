270eeebbf7e32f98692d0ca31d431de34f2f5e98
stephane conversy <conversy@gmail.com>
2020-12-15 15:58:29 +0100

    add src/gui/shapes/shapes_fwd.h

---
+++ src/gui/shapes/shapes_fwd.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace djnn {
+    class Rectangle;
+    class Circle;
+    class Ellipse;
+    class Line;
+    class Image;
+    class DataImage;
+    class Text;
+    
+    class PolyPoint;
+    class Poly;
+    class PathPoint;
+    class PathMove;
+    class PathLine;
+    class PathQuadratic;
+    class PathCubic;
+    class PathArc;
+    class PathClosure;
+    class Path;
+
+    class PathClip;
+    class RectangleClip;
+    
+    class Group;
+    class Defs;
+}
