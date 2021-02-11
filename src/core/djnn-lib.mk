29e6a7d6aa9b7c6bba20730a5f0a590c102d32cc
stephane conversy <conversy@gmail.com>
2021-02-11 11:56:04 +0100

    core - remotery make

--- src/core/djnn-lib.mk
+++ src/core/djnn-lib.mk
@@ -3,12 +3,14 @@ local_dir = $(src_dir)/core
 lib_srcs += $(local_dir)/core.cpp
 
 submodules = ontology/ control/ tree/ execution/ utils/
-ifneq ($(os),$(filter $(os),FreeRTOS em crazyflie))
+ifneq ($(os),$(filter $(os),FreeRTOS crazyflie))
 submodules += serializer/ xml/
 endif
 lib_srcs += $(call rwildcardmul,$(addprefix $(local_dir)/,$(submodules)),*.cpp)
 
-#lib_srcs += $(call rwildcard,$(local_dir)/utils/ext/remotery/,*.c)
+ifneq ($(remotery_cflags),-DRMT_ENABLED=0)
+lib_srcs += $(call rwildcard,$(local_dir)/utils/ext/remotery/,*.c)
+endif
 
 ifeq ($(os),Darwin)
 lib_pkgpath += /usr/local/opt/expat/lib/pkgconfig
@@ -18,6 +20,3 @@ endif
 ifneq ($(os),$(filter $(os),FreeRTOS em crazyflie))
 lib_pkg += expat libcurl
 endif
-
-
-
