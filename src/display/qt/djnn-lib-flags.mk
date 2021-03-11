240cf918cb3aa59d3001408a8adfe56b1b38626f
Mathieu Poirier <mathieu.poirier@enac.fr>
2021-03-11 16:17:16 +0100

    display/qt - fixed install with new qt5 brew package

--- src/display/qt/djnn-lib-flags.mk
+++ src/display/qt/djnn-lib-flags.mk
@@ -2,10 +2,13 @@ lib_cppflags += -DDJNN_USE_QT_MAINLOOP=1
 lib_cppflags += -I$(src_dir)/.. # for moc
 
 ifeq ($(os),Darwin)
-lib_pkg += Qt5OpenGL Qt5Gui
-#moc := /usr/local/opt/qt/bin/moc
-qt_dir ?= $(shell ls -dt1 /usr/local/Cellar/qt/* | head -1)
+lib_pkg +=  Qt5OpenGL Qt5Gui
+qt_dir = $(shell ls -dt1 /usr/local/Cellar/qt/5* | head -1)
+ifneq ($(qt_dir),-1)
+qt_dir = $(shell ls -dt1 /usr/local/Cellar/qt@5/* | head -1)
+endif
 lib_pkgpath += $(qt_dir)/lib/pkgconfig
+moc := $(qt_dir)/bin/moc
 endif
 
 ifeq ($(os),Linux)
