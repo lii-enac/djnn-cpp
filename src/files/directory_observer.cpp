b6d5c660f6423eb450158a05ac9d50b02a78c042
Mathieu Magnaudet <mathieu.magnaudet@enac.fr>
2021-05-22 09:19:09 +0200

    files - try to fix filesystem include issue

--- src/files/directory_observer.cpp
+++ src/files/directory_observer.cpp
@@ -12,7 +12,20 @@
  *
  */
 
-#include <filesystem>
+#ifndef __has_include
+  static_assert(false, "__has_include not supported");
+#else
+#  if __has_include(<filesystem>)
+#    include <filesystem>
+  namespace filesystem = std::filesystem;
+#  elif __has_include(<experimental/filesystem>)
+#    include <experimental/filesystem>
+  namespace filesystem = experimental::filesystem;
+#  elif __has_include(<boost/filesystem.hpp>)
+#    include <boost/filesystem.hpp>
+  namespace filesystem = boost::filesystem;
+#  endif
+#endif
 #include "files.h"
 #include "core/core-dev.h"
 #include "exec_env/global_mutex.h"
