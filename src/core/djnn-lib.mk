bc97a91f4c242ff1897d40a3a67c1845e1304b10
stephane conversy <conversy@gmail.com>
2021-08-25 17:42:39 +0200

    use brew --prefix to figure out install path, take 2

--- src/core/djnn-lib.mk
+++ src/core/djnn-lib.mk
@@ -16,8 +16,8 @@ endif
 endif
 
 ifeq ($(os),Darwin)
-lib_pkgpath += /usr/local/opt/expat/lib/pkgconfig
-lib_pkgpath += /usr/local/opt/curl/lib/pkgconfig
+lib_pkgpath += $(shell brew --prefix expat)/lib/pkgconfig
+lib_pkgpath += $(shell brew --prefix curl)/lib/pkgconfig
 endif
 
 ifneq ($(os),$(filter $(os),FreeRTOS em crazyflie))
