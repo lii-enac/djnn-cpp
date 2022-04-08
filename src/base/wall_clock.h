427ccbe468428e88d978af104b0fcac402696eac
stephane conversy <conversy@gmail.com>
2022-04-08 10:45:45 +0200

    base/wallclock: make ms optional

--- src/base/wall_clock.h
+++ src/base/wall_clock.h
@@ -53,20 +53,33 @@ namespace djnn
                 //localtime_r(&currentTime, &currentLocalTime);
                 currentLocalTime = *localtime(&currentTime); // not thread-safe
                 char timeBuffer[80];
-                std::size_t charCount { std::strftime( timeBuffer, 80,
-                                         //"%Y-%m-%d_%Hh%Mm%Ss",
-                                         ((WallClock*)get_parent())->_format.get_value().c_str(),
-                                          &currentLocalTime)
-                         };
+                string format = ((WallClock*)get_parent())->_format.get_value();
+                auto ms_pos = format.find("%k"); // our own ms formater is %k
+                if(ms_pos != string::npos) {
+                    format = format.substr(0,ms_pos) + "%" + format.substr(ms_pos,string::npos);
+                }
+                std::size_t charCount = std::strftime( timeBuffer, 80,
+                                            //"%Y-%m-%d_%Hh%Mm%Ss",
+                                            format.c_str(),
+                                            &currentLocalTime);
                 assert(charCount);
                 string res (' ', charCount);
                 std::copy(timeBuffer, timeBuffer+charCount, res.begin());
                 res.resize (charCount);
-                auto ms = now2.count() % 1000;
-                auto ms_str = djnn::to_string(ms);
-                if (ms<10) ms_str = "00" + ms_str;
-                else if (ms<100) ms_str = "0" + ms_str;
-                res += ms_str + "ms";
+
+                if(ms_pos != string::npos) {
+                    auto ms = now2.count() % 1000;
+                    auto ms_str = djnn::to_string(ms);
+                    if (ms<10) ms_str = "00" + ms_str;
+                    else if (ms<100) ms_str = "0" + ms_str;
+
+                    ms_pos = res.find("%k"); // our own ms formater is %k
+                    res = res.substr(0,ms_pos) + ms_str + res.substr(ms_pos+2, string::npos);
+                    //res.resize (charCount+1); // we have a 3-digit ms value, and we replace a 2-digit "%k" format 
+                    //std::copy(ms_pos+2, res.end()-1, &res[ms_pos+3]);
+                    //std::copy(&res[ms_pos], ms_str.begin(), ms_str.end());
+                }
+
                 _ref = res;
                 return _ref;
             }
