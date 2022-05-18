f7e315f1d76bf8b4528b1e76abab0b5e1b0f9e6d
stephane conversy <conversy@gmail.com>
2022-05-18 14:42:53 +0200

    core/utils/getset.h: GET_CHILD_VALUE returns a const & value

--- src/core/utils/getset.h
+++ src/core/utils/getset.h
@@ -5,10 +5,10 @@
 // is equivalent to:
 // auto * name = dynamic_cast<djnn::TextProperty*>(data->find("layer_name")); (this will create a djnn TextProperty* variable 'name')
 
-#define GET_CHILD_VALUE(varname, type, parent, name) auto * child_##varname = dynamic_cast<djnn::type##Property*>(parent->find_child(name)); if (!child_##varname) { fprintf (stderr, "no " #type " \"%s\" child in process %s:%s:%d\n", name, __FUNCTION__,__FILE__,__LINE__);  exit(-1); }; auto varname = child_##varname->get_value ();
+#define GET_CHILD_VALUE(varname, type, parent, name) auto * child_##varname = dynamic_cast<djnn::type##Property*>(parent->find_child(name)); if (!child_##varname) { fprintf (stderr, "no " #type " \"%s\" child in process %s:%s:%d\n", name, __FUNCTION__,__FILE__,__LINE__);  exit(-1); }; const auto& varname = child_##varname->get_value ();
 // ex: GET_CHILD_VALUE (name, djnn::Text, data, "layer_name");
 // is equivalent to:
-// auto name = dynamic_cast<djnn::TextProperty*>(data->find_child("layer_name"))->get_value(); (this will create a "string name" with the value of the property)
+// const auto& name = dynamic_cast<djnn::TextProperty*>(data->find_child("layer_name"))->get_value(); (this will create a "string name" with the value of the property)
 
 #define SET_CHILD_VALUE(type, parent, name, value, propagate) { auto * var = dynamic_cast<djnn::type##Property*>(parent->find_child(name)); if (!var) { fprintf (stderr, "no " #type " \"%s\" child in process %s:%s:%d\n", name, __FUNCTION__,__FILE__,__LINE__);  exit(-1); } var->set_value(value, propagate); }
 // ex: SET_CHILD_VALUE(djnn::Text, data, "img/path", new_path, true);
