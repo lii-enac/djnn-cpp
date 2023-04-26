33a3f42d768b6e2fd67665f53957c1991aca93ec
stephane conversy <conversy@gmail.com>
2023-04-26 18:32:12 +0200

    iofd: fix ambigous string use

--- src/exec_env/unix/iofd.h
+++ src/exec_env/unix/iofd.h
@@ -21,7 +21,7 @@
 namespace djnn {
 	class IOFD : public FatProcess, public ExternalSource {
 	public:
-		IOFD(CoreProcess* parent, const string& name, int readfd);
+		IOFD(CoreProcess* parent, const CoreProcess::string& name, int readfd);
 		virtual ~IOFD() override;
 
 		int readfd() const { return _readfd; }
