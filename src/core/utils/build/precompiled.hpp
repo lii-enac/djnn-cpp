9cbb2824159e08d1eaef1363e5f57e92a4460701
stephane conversy <conversy@gmail.com>
2023-05-14 15:10:50 +0200

    precompiled: simplify

--- src/core/utils/build/precompiled.hpp
+++ src/core/utils/build/precompiled.hpp
@@ -1,15 +1,15 @@
-#include "core/utils/containers.h"
-#include "core/utils/algorithm.h"
+// #include "core/utils/containers.h"
+// #include "core/utils/algorithm.h"
 #include "core/ontology/process.h"
 
-namespace djnn {
-    class CoreProcess;
-    class Coupling;
-}
+// namespace djnn {
+//     class CoreProcess;
+//     class Coupling;
+// }
 
-#if DJNN_STL_STD
-template class std::basic_string<char>;
-#endif
-template class djnnstl::map<djnnstl::string, djnn::CoreProcess*>;
-template class djnnstl::vector<djnn::Coupling*>;
+// #if DJNN_STL_STD
+// template class std::basic_string<char>;
+// #endif
+// template class djnnstl::map<djnnstl::string, djnn::CoreProcess*>;
+// template class djnnstl::vector<djnn::Coupling*>;
 
