54df6224b01a827d283b2f7939f83988b2f73f26
Mathieu Poirier <mathieu.poirier@enac.fr>
2024-02-08 09:52:44 +0100

    tools - improved clang-format, now automatic with vscode and has alignment

--- tools/format_clang.sh
+++ tools/format_clang.sh
@@ -3,12 +3,26 @@
 # Utiliser le premier argument comme répertoire. Si aucun argument n'est fourni, utiliser le répertoire courant.
 dir=${1:-.}
 
-# Parcourir tous les fichiers .h et .cpp dans le répertoire donné et ses sous-répertoires
-for file in $(find $dir -iname "*.h" -o -iname "*.cpp")
+# Chemins vers les fichiers d'en-tête (.h)
+HEADER_FILES=$(find $dir -type f -name "*.h")
+
+# Chemins vers les fichiers de code source (.cpp)
+CPP_FILES=$(find $dir -type f -name "*.cpp")
+
+# Appliquer clang-format aux fichiers d'en-tête avec une configuration spécifique
+for file in $HEADER_FILES
 do
-    # Appliquer clang-format au fichier
-    clang-format -i $file
+    clang-format --style=file:../src/_clang-format_header -i $file
 
     # Afficher le nom du fichier formaté
-    echo "Formatted: $file"
+    echo "header formatted: $file"
+done
+
+# Appliquer clang-format aux fichiers de code source avec une configuration différente
+for file in $CPP_FILES
+do
+    clang-format --style=file:../src/_clang-format -i $file
+
+    # Afficher le nom du fichier formaté
+    echo "src formatted: $file"
 done
