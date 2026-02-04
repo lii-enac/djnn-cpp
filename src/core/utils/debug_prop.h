baf99ea835a89ff010a810ff13388e4fe47a859e
Mathieu Poirier <mathieu.poirier@enac.fr>
2026-02-04 11:15:12 +0100

    core/utils - fixed macos compilation

--- src/core/utils/debug_prop.h
+++ src/core/utils/debug_prop.h
@@ -7,7 +7,7 @@
 //#define DBG_ACT_SEQ if (_DEBUG_SEE_PROP_SET_VALUE) cerr << -1 << " - triggers ------ " << print_process_full_name (this) << " value:" << get_value() << "\t\t" << print_process_debug_info (this) << endl;
 //#define DBG_ACT_SEQ if (_DEBUG_SEE_PROP_SET_VALUE) cerr << print_process_debug_info (this) << "\t\t\t --- " << print_process_full_name (this) << " set value to: °" << get_value() << "°" << endl;
 //#define DBG_ACT_SEQ if (_DEBUG_SEE_PROP_SET_VALUE) cerr << "\t\t\t --- " << print_process_full_name (this) << " set value from: °" << v << "° to: °" << get_value() << "°" << " propagate:" << propagate << endl;
-#define DBG_ACT_SEQ if (_DEBUG_SEE_PROP_SET_VALUE) { debug_prop_set_value (this); cerr << "set value from: °" << get_value() << "° to: °" << v << "°" << " propagate:" << propagate << endl; }
+#define DBG_ACT_SEQ if (_DEBUG_SEE_PROP_SET_VALUE) { debug_prop_set_value (this); djnnstl::cerr << "set value from: °" << get_value() << "° to: °" << v << "°" << " propagate:" << propagate << djnnstl::endl; }
 
 #else
 #define DBG_ACT_SEQ
