46ba3388a36ddd726c969224da0315b8148bea0f
stephane conversy <conversy@gmail.com>
2026-07-03 13:54:28 +0200

    input/gpio: make gpio optional since gpiod v2 is not available everywhere

--- src/input/djnn-lib.mk
+++ src/input/djnn-lib.mk
@@ -2,11 +2,20 @@ lib_djnn_deps += base core
 local_dir = $(src_dir)/input
 lib_srcs += $(local_dir)/input.cpp
 
+use_gpio ?= no
+
 ifeq ($(os),Linux)
 	lib_srcs += $(call rwildcard,src/input/linux/,*.cpp)
 	lib_djnn_deps += exec_env
 	lib_ldflags += -ludev
-	lib_pkg += libevdev libgpiod
+	lib_pkg += libevdev
+
+ifneq ($(use_gpio),no)
+	lib_pkg += libgpiod
+else
+	lib_srcs := $(filter-out src/input/linux/linux_gpio.cpp, $(lib_srcs))
+endif
+
 else
 	lib_srcs += $(local_dir)/alt/alt_input.cpp
 endif
