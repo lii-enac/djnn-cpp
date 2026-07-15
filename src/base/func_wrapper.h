356ec2f786f44bb16e7bc2ef4ad3881527bb96f8
Mathieu Poirier <mathieu.poirier@enac.fr>
2026-07-15 17:59:21 +0200

    all: update all file headers with last year's changes and contributor list (pass 3)

--- src/base/func_wrapper.h
+++ src/base/func_wrapper.h
@@ -1,4 +1,17 @@
 /*
+ *  djnn v2
+ *
+ *  The copyright holders for the contents of this file are:
+ *      Ecole Nationale de l'Aviation Civile, France (2018-2025)
+ *  See file "license.terms" for the rights and conditions
+ *  defined by copyright holders.
+ *
+ *
+ *  Contributors:
+ *      Stephane Conversy <stephane.conversy@enac.fr>
+ *
+ */
+/*
 
 function wrapper
 example usage in smala:
--- src/gui/XML/XMLGeometry.cpp
+++ src/gui/XML/XMLGeometry.cpp
@@ -1,18 +1,19 @@
 /*
- *	djnn libraries
+ *  djnn v2
  *
- *	The copyright holders for the contents of this file are:
- *		Ecole Nationale de l'Aviation Civile, France (2008-2015)
- *	See file "license.terms" for the rights and conditions
- *	defined by copyright holders.
+ *  The copyright holders for the contents of this file are:
+ *      Ecole Nationale de l'Aviation Civile, France (2018-2024)
+ *  See file "license.terms" for the rights and conditions
+ *  defined by copyright holders.
  *
- *	XML Geometry Parser
  *
- *	Contributors:
- *		Stéphane Chatty <chatty@enac.fr>
+ *  Contributors:
+ *      Stephane Chatty <Stephane.Chatty@enac.fr>
+ *      Mathieu Magnaudet <mathieu.magnaudet@enac.fr>
+ *      Mathieu Poirier <mathieu.poirier@enac.fr>
+ *      Stephane Conversy <stephane.conversy@enac.fr>
  *
  */
-
 #include <stdlib.h>
 #include <string.h>
 
