1272b643abaf1784cfc450ad11b2f90d949c41ee
Mathieu Poirier <mathieu.poirier@enac.fr>
2026-08-24 17:00:57 +0200

    color_picking.h - fix windows compilation

--- src/gui/picking/color_picking.h
+++ src/gui/picking/color_picking.h
@@ -2,7 +2,7 @@
  *  djnn v2
  *
  *  The copyright holders for the contents of this file are:
- *      Ecole Nationale de l'Aviation Civile, France (2014-2024)
+ *      Ecole Nationale de l'Aviation Civile, France (2014-2026)
  *  See file "license.terms" for the rights and conditions
  *  defined by copyright holders.
  *
@@ -15,7 +15,6 @@
  *
  */
 #pragma once
-
 #include "display/pickui.h"
 #include "display/window.h"
 #include "picking.h"
@@ -27,16 +26,17 @@ class ColorPickingView : public Picking {
     ColorPickingView (Window* win);
     virtual ~ColorPickingView ();
 
+    using pick_color_t = unsigned int;
+
     // Picking
     virtual void init ();
     PickUI*      pick (double x, double y);
     void         add_pick_shape (PickUI* gobj, bool cache = false);
     void         remove_pick_shape (PickUI* gobj);
-    virtual uint  get_pixel (unsigned int x, unsigned int y) = 0;
+    virtual pick_color_t  get_pixel (unsigned int x, unsigned int y) = 0;
     virtual void object_deactivated (PickUI* gobj);
 
     // ColorPicking
-    using pick_color_t = unsigned int; 
     pick_color_t pick_color () { return _pick_color; }
     void set_pick_color (pick_color_t c) { _pick_color = c; }
 
